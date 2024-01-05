f3640bc625c0c2b4503ff94f6a90b6a434fbc0d9
yiwen <jamesrily686@gmail.com>
2024-01-05 17:50:17 +0800

    unittest mock example

--- examples/unitest/app_test.go
+++ examples/unitest/app_test.go
@@ -1,17 +1,34 @@
 package unitest
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/cgfork/deps"
 	"github.com/cgfork/deps/deptest"
 )
 
+type mock struct {
+	deps.Implements[App]
+}
+
+func (m *mock) Display() {
+	fmt.Println("Mock is OK!!!")
+}
+
 func TestApp(t *testing.T) {
 	deptest.Test(t, deps.Config{}, func(t *testing.T, app App) {
 		app.Display()
 	})
 	deptest.Test(t, deps.Config{}, func(t *testing.T, app *app) {
+		app.Display()
+	})
+	deptest.Test(t, deps.Config{
+		Fakes: map[reflect.Type]any{
+			deps.Type[App](): &mock{},
+		},
+	}, func(t *testing.T, app App) {
 		app.Display()
 	})
 }
